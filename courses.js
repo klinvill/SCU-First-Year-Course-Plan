c2f1de0cd6ce6627ad8b608027008cdec42c4546
klinvill <kjlinvill@gmail.com>
2014-10-27 15:57:03 -0700

    Created file to hold course data
    This file will hold all the data course variables.

---
+++ courses.js
@@ -0,0 +1,12 @@
+// Course Data File
+
+// Array of courses, ordered in terms of priority placement with the highest priority being at the front
+// Each course is structured as: course = {name:”Calculus 3”, id:”MATH 13”, offered:[”F”, “W”, “S”], can_be_honors:false, waived:false, pre_req:”MATH 12”}
+var COEN_course_array = [
+    {name:"Critical Thinking & Writing 1",
+        id: "CTW 1",
+        offered:[""],
+        can_be_honors:false,
+        waived:false, pre_req:""},
+                         
+    {name:"
