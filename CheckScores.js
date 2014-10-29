08dc92bbf3a9780853e8276b97cf99c60beb2f45
klinvill <kjlinvill@gmail.com>
2014-10-29 10:07:47 -0700

    Added Jasper's changes in CheckScores.js

--- CheckScores.js
+++ CheckScores.js
@@ -12,6 +12,7 @@ function CheckWaived() {
     */
     var math11 = 0;
     var math12 = 0;
+    var math13 = 0;
     var chem11 = 0;
     var chem12 = 0;
     var chem13 = 0;
@@ -20,74 +21,83 @@ function CheckWaived() {
     var phys31 = 0;
     var phys33 = 0;
     var biol22 = 0;
-
-    //possible tests
-    var APtests = ["CalcAB", "CalcBC", "Chem", "CompSciA", "PhysicsM", "PhysicsEaM"];
+    var math9 = 1;
     
-    var testsTaken = [];
-    var scoresTaken = [];
-
-    //obtain the tests taken and their respective scores and store in variables
-    for (var j = 1; j < (Counts["AP"]+1); j++) {
-        var testIndex = document.getElementById("AP_Test_" + j).selectedIndex - 1; //-1 to account for option "Test"
-        var score = document.getElementById("AP_Score_" + j).selectedIndex;
-        testsTaken[testsTaken.length] = APtests[testIndex];
-        scoresTaken[scoresTaken.length] = score;
+    //check each AP score box and waive appropriate classes
+    var score = document.getElementById("AP_Calc_AB_Score").selectedIndex;
+    if(score > 3) {
+        math11 = 1;//waive math 11
     }
+    score = document.getElementById("AP_Calc_BC_Score").selectedIndex;
+    if(score == 3)
+        math11 = 1;//waive math 11
+    else if (score > 3) {
+        math11 = 1;
+        math12 = 1;//waive math 11 and math 12
+    }
+    score = document.getElementById("AP_Chem_Score").selectedIndex;
+    if(score == 3)
+        chem11 = 1;//waive chem11
+    else if(score == 4) {
+        chem11 = 1;
+        chem12 = 1;//waive chem11 and chem12
+    }
+    else if(score == 5) {
+        chem11 = 1;
+        chem12 = 1;
+        chem13 = 1;//waive chem11, chem12, chem13
+    }
+    score = document.getElementById("AP_Comp_Sci_Score").selectedIndex;
+    if(score == 3)
+        coen10 = 1;//waive coen10
+    else if(score > 3) {
+        coen10 = 1;
+        coen11 = 1;//waive coen 10 and coen 11
+    }
+    score = document.getElementById("AP_Mech_Score").selectedIndex;
+    if(score > 3)
+        phys31 = 1;//waive phys31
+    score = document.getElementById("AP_EnM_Score").selectedIndex;
+    if(score > 3)
+        phys33 = 1;//waive phys33
     
-    //check each test taken and see if it waives a class
-	for (var i = 0; i < testsTaken.length; i++){
-        var currentTest = testsTaken[i];
-        var currentScore = scoresTaken[i];
+
+    //check CRE
+    var CREScore = document.getElementById("CRE_Score").selectedIndex;
+    if(CREScore == 2)
+        math9 = 0;//unwaive math 9
+  
+
+    
+    //check all checkboxes
+    if(document.getElementById("OC_MATH11").checked == true)
+        math11 = 1;//waive math 11
+    if(document.getElementById("OC_MATH12").checked == true)
+        math12 = 1;//waive math 12
+    if(document.getElementById("OC_MATH13").checked == true)
+        math13 = 1;//waive math 13
+    if(document.getElementById("OC_COEN10").checked == true)
+        coen10 = 1;//waive COEN10
+    if(document.getElementById("OC_COEN11").checked == true)
+        coen11 = 1;//waive COEN11
+    if(document.getElementById("OC_PHYS31").checked == true)
+        phys31 = 1;//waive PHYS31
         
-		if(currentTest == "CalcAB"){
-			if(currentScore > 3)
-                math11 = 1;
-        }
-        else if(currentTest == "CalcBC") {
-            if(currentScore == 3)
-                math11 = 1;
-            else if (currentScore > 3) {
-                math11 = 1;
-                math12 = 1;
-            }
-        }
-        else if(currentTest == "Chem") {
-            if(currentScore == 3) 
-                chem11 = 1;
-            else if(currentScore == 4) {
-                chem11 = 1;
-                chem12 = 1;
-            }
-            else if(currentScore == 5) {
-                chem11 = 1;
-                chem12 = 1;
-                chem13 = 1;
-            }
-        }
-        else if(currentTest == "CompSciA") {
-            if(currentScore == 3)
-                coen10 = 1;
-            else if(currentScore > 3) {
-                coen10 = 1;
-                coen11 = 1;
-            }
-        }
-        else if(currentTest == "PhysicsM") {
-            if(currentScore > 3)
-                phys31 = 1;
-        }
-        else if(currentTest == "PhysicsEaM") {
-            if(currentScore > 3)
-                phys33 = 1;
-        }   
-    }
+        
+        
+        
+        
+        
     
     //alert waived classes
+    if(math9 == 1)
+        alert("Math 9 is waived.");
     if(math11 == 1)
         alert("Math 11 is waived.");
     if(math12 == 1)
         alert("Math 12 is waived.");
+    if(math13 == 1)
+        alert("Math13 is waived.");
     if(chem11 == 1)
         alert("Chem 11 is waived.");
     if(chem12 == 1)
