5c01ec8ed0ecc8e38dbc3128456f210188b73a87
JesseHarder <jhharder@scu.edu>
2014-10-30 13:41:45 -0700

    Actually adding the UpdateSchedule file because the last commit didn't.

---
+++ UpdateSchedule.js
@@ -0,0 +1,10 @@
+// Function to be called whenever something changes on the website.
+
+function updateSchedule()
+{
+    // Updated waived status of courses.
+    CheckWaived();
+    
+    // Generating a schedule.
+    generateSchedule();
+}
