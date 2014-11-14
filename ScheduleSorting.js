9fd99d11e719ed1755895c441a11edc52b54d5ca
JesseHarder <jhharder@scu.edu>
2014-11-14 14:53:21 -0800

    Progress saving for branch switch.

--- ScheduleSorting.js
+++ ScheduleSorting.js
@@ -1,6 +1,18 @@
 /* ----- File containing the logic for making the schedule look nice. ----- */
 
-var rowBasedSchedule = [[],[],[],[]];
+var rowBasedSchedule;
+
+// Function: initializes the row based schedule based on global variabls.
+
+(function initRowBasedSchedule()
+{
+    rowBasedSchedule = [];
+    
+    for (var i = 0; i < numQuarters; i++)
+    {
+        rowBasedSchedule.push([]);
+    }
+})()
 
 
 // Function: called on a course schedule to improve the look of the schedule.
@@ -10,30 +22,41 @@ function organizeSchedule(schedule)
 {
     throwIfTypeDoesNotMatch(schedule, "object", "organizeSchedule");
     
+    var subjectsToCheck = ["MATH", "COEN", "CTW", "C&I", "PHYS"]
+    
     // MATH
     var mathCoursesByQuarter = [];
     
-    for (var i = 0; i < schedule.length)
+    for (var i = 0; i < schedule.length; i++)
+    {
+        mathCoursesByQuarter.push(mathClassFromQuarter(schedule[i]));
+    }
     
-    = [mathClassFromQuarter(schedule[0]),
-                                mathClassFromQuarter(schedule[1]),
-                                mathClassFromQuarter(schedule[2])];
+    for (var i = 0; i < rowBasedSchedule.length; i++)
+    {
+        var row = rowBasedSchedule[i];
+        if (rowHasRoom(row, )
+    }
+    
     // COEN
+    var COENCoursesByQuarter = [];
     
-    // CTW
-    
-    // CNI
-    
-    // PHYS
+    for (var i = 0; i < schedule.length; i++)
+    {
+        COENCoursesByQuarter.push(courseOfTypeFromQuarter("COEN", schedule[i]));
+    }
 }
 
-/* --- Functions for removing courses from the schedule --- */
+/* --- Schedule Information Getters and Setters --- */
 
 // Function: removes the given course from the schedule.
 // Parameters: a course to be removed and the schedule.
 // Return Value: Boolean for whether course was found in schedule.
 function removeCourseFromSchedule(tempCourse, schedule)
 {
+    throwIfTypeDoesNotMatch(tempCourse, "object", "removeCourseFromSchedule");
+    throwIfTypeDoesNotMatch(schedule, "object", "removeCourseFromSchedule");
+    
     for (var i = 0; i < schedule.length; i++)
     {
         var quarter = schedule[i];
@@ -51,6 +74,53 @@ function removeCourseFromSchedule(tempCourse, schedule)
     return false;
 }
 
+// Function: counts the number of non-undefined values in the array.
+// Parameter: a row of courses.
+// Return Value: number of courses in the row array.
+function numCoursesInRow(row)
+{
+    throwIfTypeDoesNotMatch(row, "object", "numCoursesInRow");
+    
+    var count = 0;
+    
+    for (var i = 0; i < row.length; i++)
+    {
+        if (row[i] != undefined) count++;
+    }
+    
+    return count;
+}
+
+// Function: called to check if a given row has room for all the courses in a list.
+// Parameters: a row array and a list of courses array.
+// Return value: Boolean.
+
+function rowHasRoom(row, courses)
+{
+    
+}
+
+/* --- Extracting Data from Schedule or Quarter --- */
+
+
+// Function: called to get a course of given type out of a quarter.
+// Parmaeters: a type string and a quarter array of courses.
+// Return value: a course of given type if one is present in quarter. Undefined otherwise.
+function courseOfTypeFromQuarter(type, quarter)
+{
+    throwIfTypeDoesNotMatch(type, "string", "courseOfTypeFromQuarter");
+    throwIfTypeDoesNotMatch(quarter, "object", "courseOfTypeFromQuarter");
+    
+    for (var i = 0; i < quarter.length; i++)
+    {
+        var tempCourse = quarter[i];
+        if (courseIsType(type))
+            return tempCourse;
+    }
+    
+    return undefined;
+}
+
 // Function: called to get a math course out of a quarter.
 // Parmaeter: a quarter array of courses.
 // Return value: a math course if one is present. Undefined otherwise.
@@ -62,7 +132,7 @@ function mathClassFromQuarter(quarter)
     {
         var tempCourse = quarter[i];
         if (courseIsType("MATH") || courseIsType("AMTH"))
-            return tempCourse
+            return tempCourse;
     }
     
     return undefined;
