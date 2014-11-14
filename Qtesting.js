ce45dbbfe5e6a71b37b20ca15e7511ba90c971fd
klinvill <kjlinvill@gmail.com>
2014-11-13 16:51:56 -0800

    Started unit testing

---
+++ Qtesting.js
@@ -0,0 +1,9 @@
+function runUnitTests()
+{
+    Qunit.test("MATH Course Array Tests", function(assert) {
+               $('#Major').class
+               assert.equal(
+               
+               
+               }
+}
