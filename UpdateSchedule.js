0a02e15099c662c4aeaa78cc76ea8fc17dc3c5d2
JesseHarder <jhharder@scu.edu>
2014-11-13 20:06:37 -0800

    Fixed rediculous syntax error problem.

--- UpdateSchedule.js
+++ UpdateSchedule.js
@@ -14,8 +14,7 @@ function createCheckboxes()
     $.each(tempCourseArray, function(index, value){
            if (value.shouldHaveCheckBox)
            {
-                var htmlString ='<label id="Label_'+value.id+'" style="display:none"><input type="checkbox" onclick="checkboxClicked("'+value.id+'")" name="Other_Waived" id="OC_'+value.id+'" value="'+value.id+'">'+value.id+'</label><br style="display:none"/>';
-           
+                var htmlString ='<label id="Label_'+value.id+'" style="display:none"><input type="checkbox" onclick="checkboxClicked('+"'"+value.id+"'"+')" name="Other_Waived" id="OC_'+value.id+'" value="'+value.id+'">'+value.id+'</label><br style="display:none"/>';
                 // Add new checkbox object to checkbox javascript array
                 addCheckBox(value.id, 0, 0);
            
@@ -44,6 +43,7 @@ function createCheckboxes()
 
 function checkboxClicked(courseID)
 {
+    console.log("Called.");
     debugger;
     var checkboxElement = getHTMLCheckBoxElementForCourseID(courseID);
     if (checkboxElement != undefined)
